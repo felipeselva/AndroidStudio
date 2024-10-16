57ddee2d36ea7ccaea3ca81fd256c5f36e68e7c2
Felipe Selva ☠ <sellvafelipe@gmail.com>
2024-10-16 17:16:19 -0300

    Retornar Extra no Intent

--- IntentComExtras/app/src/main/java/com/example/intentcomextras/MainActivity.kt
+++ IntentComExtras/app/src/main/java/com/example/intentcomextras/MainActivity.kt
@@ -2,23 +2,46 @@ package com.example.intentcomextras
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.example.intentcomextras.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var  result: ActivityResultLauncher<Intent>
+    private var nome ="";
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
     binding = ActivityMainBinding.inflate(layoutInflater)
+
         setContentView(binding.root)
 
-        binding.buttonOk.setOnClickListener{
-            val nome = binding.editNome.text.toString()
+        binding.buttonMudarNome.setOnClickListener{
+
 
             val i = Intent(this, MainActivity2::class.java)
+
             i.putExtra("nome", nome)
-            startActivity(i);
+
+            //startActivity(i);
+            result.launch(i);
+        }
+
+        result = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if(it.data != null && it.resultCode == 1) {
+                nome = it.data?.getStringExtra("nome").toString()
+                binding.editNome.text = "Olá ${nome}"
+
+
+            } else {
+                Toast.makeText(applicationContext,"Erro ao atualizar o nome", Toast.LENGTH_SHORT).show()
+            }
         }
 
     }
--- IntentComExtras/app/src/main/java/com/example/intentcomextras/MainActivity2.kt
+++ IntentComExtras/app/src/main/java/com/example/intentcomextras/MainActivity2.kt
@@ -1,7 +1,6 @@
 package com.example.intentcomextras
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.intentcomextras.databinding.ActivityMain2Binding
 
@@ -10,19 +9,29 @@ class MainActivity2 : AppCompatActivity() {
 private lateinit var binding: ActivityMain2Binding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
 
     binding = ActivityMain2Binding.inflate(layoutInflater)
+
         setContentView(binding.root)
 
         val i = intent
         val nome = i.extras?.getString("nome")
 
-        if (nome.equals("") || nome == null) {
+        binding.editNome.setText(nome)
+
+        binding.buttonOk.setOnClickListener{
+            i.putExtra("nome", binding.editNome.text.toString())
+            setResult(1,i)
+            finish()
+        }
+
+        /*if (nome.equals("") || nome == null) {
             Toast.makeText(applicationContext, "Nome Não inserido", Toast.LENGTH_SHORT).show()
         } else {
             binding.textNome.setText("Olá ${nome}")
-        }
+        }*/
 
     }
 }
